a2977201e1a538481abdf1c56b1612a9aeb37422
berrythewa <waberryd@proton.me>
2024-12-16 18:23:11 +0100

    advent of code 2024 day1,2 and 3

---
+++ day1/target/debug/day1.d
@@ -0,0 +1,1 @@
+/home/berry/Development/advent2024/day1/target/debug/day1: /home/berry/Development/advent2024/day1/src/main.rs
---
+++ day1/target/debug/deps/day1-754d9a61e80a487e.d
@@ -0,0 +1,5 @@
+/home/berry/Development/advent2024/day1/target/debug/deps/day1-754d9a61e80a487e: src/main.rs
+
+/home/berry/Development/advent2024/day1/target/debug/deps/day1-754d9a61e80a487e.d: src/main.rs
+
+src/main.rs:
